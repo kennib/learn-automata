d06be876a556ded57503755cfa310d91ed7fbdce
kennib <kenni.bawden@gmail.com>
2013-07-09 18:55:16 +1000

    Updated the example to use range() instead of enumerate()

--- automata.py
+++ automata.py
@@ -1,56 +1,54 @@
 import copy
 import graphics
 
-number_of_iterations = 10
-display_pause_time = 1000
+number_of_iterations = 100
+display_pause_time = 100
 
 grid = [[1,0,0,1,1,0,0,1,1,1,0,0,1,0,0,1,1,0,0,1],[0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,0],[0,0,1,1,0,0,1,1,0,0,0,0,0,1,0,1,0,1,0,1],[0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,0,0],[0,1,0,0,0,1,1,0,1,1,1,0,1,1,1,0,0,1,1,1],[0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,1,0,1,1,1],[0,0,1,1,1,0,1,0,0,0,1,1,1,0,0,0,0,1,1,1],[0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,1,0,0,0],[1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0],[1,1,0,1,1,0,0,0,0,1,0,0,0,0,1,0,1,1,0,0],[1,0,1,1,0,0,0,0,1,0,1,0,1,1,0,0,1,1,1,0],[1,0,1,1,0,0,0,1,1,0,0,1,0,0,1,0,0,0,0,1],[1,1,0,0,0,1,1,0,0,0,1,0,0,0,0,0,0,1,1,0],[0,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,1,1,0,0],[1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0]]
 
-display = graphics.SimpleDisplay("2D Cellular Automaton", len(grid[0]), len(grid), scale=30)
-
-alive = 1
-dead = 0
+display = graphics.SimpleDisplay("2D Cellular Automaton", len(grid[0]), len(grid), autoflush=False)
 
 # For each iteration...
 for iteration in range(number_of_iterations):
+    # Keep a copy of the previous iteration
+    previous_grid = copy.deepcopy(grid)
+    
+    # For each cell in the grid...
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            cell = grid[row][column]
 
-  # Keep a copy of the previous iteration
-  previous_grid = copy.deepcopy(grid)
+            # Display the cells
+            if cell == 1:
+                display.plot(column, row, "black")
+            else:
+                display.plot(column, row, "white")
 
-  # For each cell in the grid...
-  for row, row_of_cells in enumerate(grid):
-    for column, cell in enumerate(row_of_cells):
-        
-      # Display the cell
-      if cell is alive:
-        display.plot(column, row, "black")
-      else:
-        display.plot(column, row, "white")
+            # Count the number of living neighbours in the previous_grid
+            living_neighbours = 0
+            for r in [row-1, row, row+1]:
+                for c in [column-1, column, column+1]:
+                    # Check the neighbours are in bounds and are not the cell itself
+                    if r > 0 and r < len(grid) and \
+                       c > 0 and c < len(grid[0]) and \
+                       not(r is row and c is column):
+                        neighbour = previous_grid[r][c]
+                        if neighbour == 1:
+                            living_neighbours += 1
 
-      # Count the number of living neighbours
-      living_neighbours = 0
-      for r in [row-1, row, row+1]:
-        for c in [column-1, column, column+1]:
-          if r > 0 and r < len(grid) and \
-             c > 0 and c < len(grid[0]) and \
-             not(r is row and c is column):
-            neighbour = previous_grid[r][c]
-            if neighbour is alive:
-                living_neighbours += 1
+            # Update the state of the cell
+            if cell == 1:
+                if living_neighbours == 2 or living_neighbours == 3:
+                    grid[row][column] = 1
+                else:
+                    grid[row][column] = 0
+            else:
+                if living_neighbours == 3:
+                    grid[row][column] = 1
+                else:
+                    grid[row][column] = 0
 
-      # Determine if the cell is living or dead in this iteration
-      if cell is alive:
-        if living_neighbours is 2 or living_neighbours is 3:
-          grid[row][column] = alive
-        else:
-          grid[row][column] = dead
-      else:
-        if living_neighbours is 3:
-          grid[row][column] = alive
-        else:
-          grid[row][column] = dead
-
-  
-  display.pause(display_pause_time)
-  
-  display.start()
+    display.update()
+    display.pause(display_pause_time)
+    
+display.begin()
